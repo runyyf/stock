b3a345b52227c8189ad4daa38e57110d2ab48bf6
runyyf <38844996@qq.com>
2018-12-25 16:22:17 +0800

    创建项目

---
+++ db/sys_schema.sql
@@ -0,0 +1,36 @@
+-- CREATE DATABASE springboot DEFAULT CHARACTER SET utf8;
+
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS stock;
+
+-- 用户表
+CREATE TABLE user(
+  id BIGINT AUTO_INCREMENT COMMENT '编号',
+  username VARCHAR(100) COMMENT '用户名',
+  password VARCHAR(100) COMMENT '密码',
+  PRIMARY KEY(id)
+) CHARSET=utf8 ENGINE=InnoDB;
+
+INSERT INTO user VALUES(1, 'tycoding', '123');
+INSERT INTO user VALUES(2, '涂陌', '123');
+
+-- 商品表
+CREATE TABLE stock(
+  id BIGINT AUTO_INCREMENT COMMENT '编号',
+  name VARCHAR(100) COMMENT '名称',
+  gid VARCHAR(100) COMMENT '股票代码',
+  incre_per VARCHAR(100) COMMENT '涨跌幅百分比',
+  increase VARCHAR(100) COMMENT '涨跌额',
+  today_start_pri VARCHAR(100) COMMENT '今日开盘价',
+  yesterday_end_pri VARCHAR(100) COMMENT '昨日收盘价',
+  now_pri VARCHAR(100) COMMENT '当前价格',
+  today_max VARCHAR(100) COMMENT '今日最高价',
+  today_min VARCHAR(100) COMMENT '今日最低价',
+  pic_min_url VARCHAR(300) COMMENT '分时k线图',
+  pic_day_url VARCHAR(300) COMMENT '日k线图',
+  pic_week_url VARCHAR(300) COMMENT '周k线图',
+  pic_month_url VARCHAR(300) COMMENT '月k线图',
+  date_created datetime NOT NULL COMMENT '创建时间',
+  date_last_update datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY(id)
+) CHARSET=utf8 ENGINE=InnoDB;
